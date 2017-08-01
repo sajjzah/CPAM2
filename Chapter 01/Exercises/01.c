10dfd4c698543b917845c64a803c81653e9f3216
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2017-08-02 01:23:57 +0430

    Create 01.c

--- Chapter 01/Exercises/01.c
+++ Chapter 01/Exercises/01.c
@@ -1,2 +1,4 @@
-// Dependent on compiler, you may get a warning message or not.
-// main function has int return type but return statement is missing.
+/*
+Dependent on compiler, you may get a warning message or not.
+main function has int return type but return statement is missing.
+*/
