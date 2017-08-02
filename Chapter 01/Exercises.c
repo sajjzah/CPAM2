d90daed33233376275c1c2b4c214d51f9f7bda46
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2017-08-02 10:41:27 +0430

    Create Exercises.c

--- Chapter 01/Exercises.c
+++ Chapter 01/Exercises.c
@@ -92,7 +92,7 @@ answer = (3 * q - p * p) / 3;
 an one line command and everything in this line will be known as directive.
 (that's why we don't put ; at the end of directives: they don't need that!)
 
-- The spaces between data type (int) and identifires (main, height) are esential because
+- The spaces between data type (int) and identifires (main, height) are essential because
 otherwise they'll got mixed together and produce new (and probably meaningless) token.
 
 - The space between "return 0". the reason is same as the above.
