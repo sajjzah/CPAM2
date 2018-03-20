fadc5a9c136476e9f0fbd716c1b664c41d0a776b
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-03-20 22:34:48 +0330

    Create ex 02.c

---
+++ Ch 02/Exercises/ex 02.c
@@ -0,0 +1,11 @@
+// Directives:
+//     #include <stdio.h>
+// Statements:
+//     printf("Parkinson's Law:\nWork expands as to ");
+//     printf("fill the time\n");
+//     printf("available for its completion.\n");
+//     return 0;
+// Output:
+//     Parkinson's Law:
+//     Work expands as to fill the time
+//     available for its completion.
