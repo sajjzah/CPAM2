3d318bfdf7f73b37ad7979948eb791279fa902a4
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-03-21 02:20:03 +0330

    Create ex07.c

---
+++ Ch 02/Exercises/ex07.c
@@ -0,0 +1,3 @@
+// (a) and (e) are keywords.
+// An easy way to determine if a word is keyword or not is to declare a variable
+// with that name. if you haven't got any compile-time error, then it's not a keyword.
