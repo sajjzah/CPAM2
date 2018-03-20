89ca654995e5885396d917a85bbe505f10cb4d4f
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-03-21 02:51:22 +0330

    Create pp01.c

---
+++ Ch 02/Programming Projects/pp01.c
@@ -0,0 +1,18 @@
+// prints a simple picture
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("       *\n");
+	printf("      * \n");
+	printf("     *  \n");
+	printf("*   *   \n");
+	printf(" * *    \n");
+	printf("  *     \n");
+
+	return 0;
+}
+
+// Note that we could draw the entire picture with a single call of printf
+// function, but it's easier and more readable to use it multiple times.
