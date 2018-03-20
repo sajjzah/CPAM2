3509abd1ec146f33397e1fe714e4d75ff3a78130
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-03-21 02:26:49 +0330

    Create ex10.c

---
+++ Ch 02/Exercises/ex10.c
@@ -0,0 +1,6 @@
+// - The new line after #include <stdio.h> is essential because directives are known as
+// one line command and everything in this line will be known as directive.
+// (that's why we don't put ; at the end of directives: they don't need that!)
+// - The spaces between data type (int) and identifires (main, height) are essential because
+// otherwise they'll got mixed together and produce new (and meaningless) token.
+// - The space between "return 0". the reason is same as the above.
