1963c83ac42fd6943280b184dd254d1136072532
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-03-20 22:22:15 +0330

    Create ex 01.c

---
+++ Ch 02/Exercises/ex 01.c
@@ -0,0 +1,3 @@
+// Dependent on compiler, you may get a warning message or not. Because
+// main function has int return type but return statement is missing.
+// (I didn't get warning message on GNU GCC compiler)
