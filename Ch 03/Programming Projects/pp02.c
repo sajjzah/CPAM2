d49b460ea2c5748f6ea1f74d3b2e3d3b4b52a547
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-03-26 22:07:20 +0430

    Create pp02.c

---
+++ Ch 03/Programming Projects/pp02.c
@@ -0,0 +1,20 @@
+// formats product information entered by the user
+
+#include <stdio.h>
+
+int main(void)
+{
+    int item_number, purchase_day, purchase_month, purchase_year;
+    float unit_price;
+
+    printf("Enter item number: ");
+    scanf("%d", &item_number);
+    printf("Enter unit price: ");
+    scanf("%f", &unit_price);
+    printf("Enter purchase date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &purchase_month, &purchase_day, &purchase_year);
+
+    printf("Item\t\tUnit\t\tPurchase\n\t\tPrice\t\tDate\n");
+    printf("%-d\t\t$%7.2f\t\t%-.2d/%-.2d/%-.4d", item_number,
+           unit_price, purchase_month, purchase_day, purchase_year);
+}
