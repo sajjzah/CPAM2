63f6451b795bda86af1dd1a8ac61bee726d69591
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-03-26 07:22:03 +0430

    Create ex03.c

---
+++ Ch 03/Exercises/ex03.c
@@ -0,0 +1,9 @@
+// (a): Equivalent: compiler will skip all the blanks
+//      before the number in both strings.
+// (b): Different: in the first one after reading first number
+//      compiler tries to immediately match - with the input.
+//      i.e. the second one allows blanks before matching -
+// (c): Different: The second format string causes program to
+//      skip all blank inputs after reading number (including
+//      new line) and wait for a non-blank input.
+// (d): Equivalent: both skip the blanks after comma.
