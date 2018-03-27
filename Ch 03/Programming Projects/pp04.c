e3bbc5ece7333e16ee6a303e8312aed1c3862e3e
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-03-27 23:56:16 +0430

    Create pp04.c

---
+++ Ch 03/Programming Projects/pp04.c
@@ -0,0 +1,15 @@
+// displays the telephone number entered in the different format
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num2, num3;
+
+	printf("Enter phone number [(xxx) xxx-xxxx]: ");
+	scanf("(%d) %d-%d", &num1, &num2, &num3);
+
+	printf("You entered %.3d.%.3d.%.4d\n", num1, num2, num3);
+
+	return 0;
+}
