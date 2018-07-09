946350f13bb366fefa4508f46644d48cb7168bfb
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-07-09 07:36:09 +0430

    Create ex13.c

---
+++ Ch 04/Exercises/ex13.c
@@ -0,0 +1,6 @@
+// ++i. i++ and (i += 1) produce different values.
+// for example, when i = 1:
+// if we write j = (i += 1), then j = 2
+// if we write j = (++i), then j = 2
+// if we write j = (i++), then j = 1
+ 
