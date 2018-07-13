22424d5377f6df8b83d54910ac95e1cfca4f08b1
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-07-13 14:25:49 +0430

    Create ex06.c

---
+++ Ch 05/Exercises/ex06.c
@@ -0,0 +1,4 @@
+// It's legal but doesn't have the meaning stated in printf function.
+// because of the lower precedence of equality operation from subtraction,
+// the program first calculates 1-10 which is 9. then checks if n is equal to 9 or not.
+// printf function only executes when n is equal to 9.
