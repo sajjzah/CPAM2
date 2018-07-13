956477fae68ab6834122b1b35da8b8d25ed870f5
Sajjad Z <30619560+sajjzah@users.noreply.github.com>
2018-07-13 14:25:16 +0430

    Create ex05.c

---
+++ Ch 05/Exercises/ex05.c
@@ -0,0 +1,6 @@
+// It's legal but doesn't have the meaning stated in printf function.
+// the program first check if n in greater than or equal to 1. the result is
+// 0 or 1. the checks if the result is less than or equal to 10. if it is,
+// prints the string. so when n = 5, (n >= 1) will be true (1). and then
+// (1 <= 10) will be true. so printf function will be executed.
+// In fact, for any value of n, the if statement will be executed.
